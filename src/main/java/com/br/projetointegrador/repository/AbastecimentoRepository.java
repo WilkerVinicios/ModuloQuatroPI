bda5d9270e5c6a4acf619450de1d400785373233
wilker <willkervinicios@gmail.com>
2019-12-06 17:14:07 -0300

    Repository WebServices Abastecimento Usuario Veiculos

---
+++ src/main/java/com/br/projetointegrador/repository/AbastecimentoRepository.java
@@ -0,0 +1,7 @@
+package com.br.projetointegrador.repository;
+
+import com.br.projetointegrador.model.Abastecimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbastecimentoRepository extends JpaRepository<Abastecimento,Long> {
+}
---
+++ src/main/java/com/br/projetointegrador/repository/VeiculosRepository.java
@@ -0,0 +1,7 @@
+package com.br.projetointegrador.repository;
+
+import com.br.projetointegrador.model.Veiculos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VeiculosRepository extends JpaRepository<Veiculos,Long> {
+}
